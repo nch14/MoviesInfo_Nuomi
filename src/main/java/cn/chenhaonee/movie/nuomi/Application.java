6baf1ebd8709d1a8d48718bb674592692cfd1332
chenhao.ni <chenhaonee@outlook.com>
2017-06-12 14:08:48 +0800

    基本完事了

---
+++ src/main/java/cn/chenhaonee/movie/nuomi/Application.java
@@ -0,0 +1,14 @@
+package cn.chenhaonee.movie.nuomi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by chenhaonee on 2017/6/9.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class,args);
+    }
+}
